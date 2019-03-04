14aedb07f5f78fd68b2b77a786a50c3c1e831837
Ivan <4esoquevedoi@gmail.com>
2019-03-04 22:43:39 +0100

    password strength checker feature added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const main = () => {
-  const passStrengthChecker = require('zxcvbn');
+  // const passStrengthChecker = require('zxcvbn');
 
   let strength = {
     0: 'Worst',
@@ -10,6 +10,25 @@ const main = () => {
     3: 'Good',
     4: 'Strong'
   };
+
+  const password = document.getElementById('pass');
+  const meter = document.getElementById('password-strength-meter');
+  const strengthText = document.getElementById('password-strength-text');
+
+  password.addEventListener('input', function () {
+    let val = password.value;
+    let result = zxcvbn(val);
+
+    // Update the password strength meter
+    meter.value = result.score;
+
+    // Update the text indicator
+    if (val !== '') {
+      strengthText.innerHTML = 'Strength: ' + strength[result.score];
+    } else {
+      strengthText.innerHTML = '';
+    }
+  });
 };
 
 window.addEventListener('load', main);
