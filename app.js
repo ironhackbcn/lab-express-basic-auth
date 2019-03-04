c6a15496859d955952efc499d60b3192551306ab
franlol <fraaaaan@gmail.com>
2019-03-04 17:10:09 +0100

    First commit, project ready to work

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -10,10 +10,11 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 mongoose.connect('mongodb://localhost/basic-auth', {
-  keepAlive: true,
-  useNewUrlParser: true,
-  reconnectTries: Number.MAX_VALUE
+    keepAlive: true,
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE
 });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -30,20 +31,20 @@ app.use('/', indexRouter);
 
 // NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found');
+    res.status(404);
+    res.render('not-found');
 });
 
 // NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // always log the error
-  console.error('ERROR', req.method, req.path, err);
+    // always log the error
+    console.error('ERROR', req.method, req.path, err);
 
-  // only render if the error ocurred before sending the response
-  if (!res.headersSent) {
-    res.status(500);
-    res.render('error');
-  }
+    // only render if the error ocurred before sending the response
+    if (!res.headersSent) {
+        res.status(500);
+        res.render('error');
+    }
 });
 
 module.exports = app;
