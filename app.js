9e99eec9bbedd5facc0aed897c4c69f9599fd57c
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-03-04 17:42:28 +0100

    signup done

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+const flash = require('connect-flash');
+const hbs = require('hbs');
 
 const app = express();
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+app.use(flash());
 
 mongoose.connect('mongodb://localhost/basic-auth', {
   keepAlive: true,
@@ -23,8 +42,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 // -- 404 and error handler
 
