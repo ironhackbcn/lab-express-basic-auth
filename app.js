5fd8ae3fbe8283928670c5c0b345b43eb66a208c
Llorenç <crack.one.7@gmail.com>
2019-03-04 22:53:13 +0100

    falla flash

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+
 const cookieParser = require('cookie-parser');
+
 const logger = require('morgan');
+
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 const indexRouter = require('./routes/index');
+const signupRouter = require('./routes/signup');
+const loginRouter = require('./routes/login');
+
+const flash = require('connect-flash');
+
+const hbs = require('hbs');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/basic-auth', {
+// Cookies
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+// Para mostras mensages
+app.use(flash());
+
+// conexions von base de datos
+mongoose.connect('mongodb://localhost/logsign', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
@@ -21,10 +49,16 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// Rutas de las carpetas
 app.use('/', indexRouter);
+app.use('/auth', signupRouter);
+app.use('/auth', loginRouter);
 
 // -- 404 and error handler
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/', (req, res, next) => {
+  res.redirect('/');
 });
 
 module.exports = router;
