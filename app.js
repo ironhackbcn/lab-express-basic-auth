473b423db931a1d9f3975f87a2c5adf96ed00205
Alex Bruster <eljineteradiactivo@gmail.com>
2019-03-04 22:03:57 +0100

    add signup and login

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/basic-auth', {
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+mongoose.connect('mongodb://localhost/ejemplo-auth', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
@@ -23,6 +38,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Makes the currentUser available in every page
+// note1: currentUser needs to match whatever you use in login/signup/logout routes
+// note2: if using passport, req.user instead
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
+const saltRounds = 10;
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+// sign up
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+
+router.post('/signup', async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.redirect('/signup');
+    return;
+  }
+  try {
+    const validation = await User.findOne({ username });
+    if (validation) {
+      res.redirect('/signup');
+      return;
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const newUser = {
+      username,
+      password: hashedPassword
+    };
+    const createdUser = await User.create(newUser);
+    req.session.currentUser = createdUser;
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+});
+
+// log in
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.redirect('/login');
+    return;
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      res.redirect('/login');
+      return;
+    }
+    if (bcrypt.compareSync(password /* provided password */, user.password/* hashed password */)) {
+      // Save the login in the session!
+      req.session.currentUser = user;
+      res.redirect('/');
+    } else {
+      res.redirect('/login');
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
