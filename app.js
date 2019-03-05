80ab879bc52bc856b1f2d07d5ad85e7d2394b020
stellarwaves <matias.salasdev@gmail.com>
2019-03-05 08:57:48 +0100

    iteracion 1 y 2

--- app.js
+++ app.js
@@ -1,49 +1,78 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+// const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index')
+const authRouter = require('./routes/auth')
 
-const app = express();
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+const app = express()
+
+// Cookies cheat sheet
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
+
+// after the session middleware add the flash middleware
+app.use(flash())
 
 mongoose.connect('mongodb://localhost/basic-auth', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
-});
+})
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+// Reconoce a los usuarios logueados y muestra información segun
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser
+  next()
+})
+
+app.use('/', indexRouter)
+app.use('/auth', authRouter)
 
 // -- 404 and error handler
 
 // NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found');
-});
+  res.status(404)
+  res.render('not-found')
+})
 
 // NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err);
+  console.error('ERROR', req.method, req.path, err)
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500);
-    res.render('error');
+    res.status(500)
+    res.render('error')
   }
-});
+})
 
-module.exports = app;
+module.exports = app
