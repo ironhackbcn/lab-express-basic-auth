22139f40102b2459f6d5d51362560dd7cd7e6777
Sergio Cordero <sergio.crol@gmail.com>
2019-07-23 19:52:35 +0200

    First commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const createError = require('http-errors');
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -14,6 +19,26 @@ mongoose.connect('mongodb://localhost/basic-auth', {
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
 });
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,8 +48,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 // -- 404 and error handler
 
