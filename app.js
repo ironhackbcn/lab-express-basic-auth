1dfb1b9b1b14542842b0b27307f1ed89ad346c1a
Pol Ribas <polribasrovira@gmail.com>
2019-07-23 18:53:25 +0200

    Adri and Pol Rock it

--- app.js
+++ app.js
@@ -1,49 +1,69 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+'use strict'
 
-const indexRouter = require('./routes/index');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const hbs = require('hbs')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-const app = express();
+const indexRouter = require('./routes/index')
+const authRouter = require('./routes/auth')
 
-mongoose.connect('mongodb://localhost/basic-auth', {
+const app = express()
+
+mongoose.connect('mongodb://localhost/labAuth', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
-});
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-// -- 404 and error handler
-
-// NOTE: requires a views/not-found.ejs template
-app.use((req, res, next) => {
-  res.status(404);
-  res.render('not-found');
-});
-
-// NOTE: requires a views/error.ejs template
-app.use((err, req, res, next) => {
-  // always log the error
-  console.error('ERROR', req.method, req.path, err);
-
-  // only render if the error ocurred before sending the response
-  if (!res.headersSent) {
-    res.status(500);
-    res.render('error');
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  }),
+  secret: '-b&$ll..´´//%99$jou--jou/Grizzly$$@',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
   }
-});
+}))
 
-module.exports = app;
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser
+  next()
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/auth', authRouter)
+
+app.use((req, res, next) => {
+  res.status(404)
+  res.redirect('/')
+})
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err)
+
+  if (!res.headersSent) {
+    res.status(500)
+    res.render('error')
+  }
+})
+
+module.exports = app
