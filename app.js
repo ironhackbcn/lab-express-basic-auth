d0281894a1afe8ecb87731666ee4d64033b07525
Nicolas Urman <urmannicolas@gmail.com>
2019-07-23 21:59:10 +0200

    the first and the last commit

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const createError = require('http-errors');
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const hbs = require('hbs');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/basic-auth', {
+mongoose.connect('mongodb://localhost/authLab', {
   keepAlive: true,
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE
 });
-// view engine setup
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,21 +50,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
-// -- 404 and error handler
-
-// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
   res.status(404);
-  res.render('not-found');
+  res.redirect('/');
 });
 
-// NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
-  // always log the error
   console.error('ERROR', req.method, req.path, err);
 
-  // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500);
     res.render('error');
