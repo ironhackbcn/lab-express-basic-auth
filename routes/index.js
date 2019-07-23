8b1cfb3edcfb4b440a27b79ccbeb1bba84731c0b
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-23 21:06:47 +0200

    create sign up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
+const User = require('../models/User');
+
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/signUp', (req, res, next) => {
+  res.render('signUp');
+});
+
+router.post('/newUser', async (req, res, next) => {
+  try {
+  const { username, password } = req.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  const user = await User.findOne({ username });
+  if (user) {
+    return res.redirect('/');
+  }
+  const newUser = await User.create({
+    username,
+    password: hashedPassword
+  });
+
+  req.session.currentUser = newUser;
+  res.redirect('/');
+} catch (error) {
+  next(error);
+}
+});
+
 
 module.exports = router;
