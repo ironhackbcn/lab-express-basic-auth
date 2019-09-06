200a6ba8ee21a126a7c385085c2fc128555ca9a0
Tash Tenner <tashfonetikas@gmail.com>
2019-09-06 20:40:59 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const checkIfLoggedIn = require("../middlewares/auth");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/loggedin", checkIfLoggedIn, (req, res, next) => {
+  res.render("loggedin", { title: "Express" });
 });
 
 module.exports = router;
